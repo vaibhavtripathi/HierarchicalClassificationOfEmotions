ddd0926291d9d5acc70e4e00060dc6cf4ec703a0
Vaibhav <vtripathi@cse.iitb.ac.in>
2016-06-30 01:48:49 +0530

    First

--- ResultsGen.py
+++ ResultsGen.py
@@ -5,7 +5,7 @@ testdirectory = './'
 for file in os.listdir(testdirectory):
     current_file = os.path.join(testdirectory, file)
     if 'pred' in current_file:
-    	os.system("paste test_labels.txt "+current_file+" > labelfile.txt")
+    	os.system("paste fk-test-label.txt "+current_file+" > labelfile.txt")
     	resfile = 'res'+''.join(ch for ch in current_file if ch in '1234567890')+'.txt'
     	os.system("python ClassificationReport.py labelfile.txt > "+resfile)
 
